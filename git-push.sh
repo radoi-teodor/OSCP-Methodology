11be4839f3e21ac52133889d345da42a9e94511e
Your Name <you@example.com>
2023-07-15 06:17:37 -0400

    Added output on git automator

--- git-push.sh
+++ git-push.sh
@@ -2,5 +2,6 @@
 php8.0 $(pwd)/cherrytreetomarkdown/cherrytomd.php -a -t $(pwd)/OSCP-Methodology.ctd $(pwd)
 git add *
 git commit -m "${1:-'Default Message'}"
+echo "Git access token: "
 read -s git_access_token
 git push https://$git_access_token@github.com/radoi-teodor/OSCP-Methodology.git
