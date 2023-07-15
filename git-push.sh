4085c5a476cea308f03a2c5b71f97726b0e31855
Your Name <you@example.com>
2023-07-15 06:22:48 -0400

    Fine tuned git automator

--- git-push.sh
+++ git-push.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-rm index.md
-rm README.md
-rm -rf images
-rm -rf files
+rm index.md 2>/dev/null
+rm README.md 2>/dev/null
+rm -rf images 2>/dev/null
+rm -rf files 2>dev/null
 
 php8.0 $(pwd)/cherrytreetomarkdown/cherrytomd.php -a -t $(pwd)/OSCP-Methodology.ctd $(pwd)
-git add *
+git add -A
 git commit -m "${1:-'Default Message'}"
 echo -n "Git access token: "
 read -s git_access_token
