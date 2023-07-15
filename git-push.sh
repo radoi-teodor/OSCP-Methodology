1c6ba44ec315822ef08c1bb1c477e7b940005f77
Your Name <you@example.com>
2023-07-15 06:23:00 -0400

    Fine tuned git automator

--- git-push.sh
+++ git-push.sh
@@ -3,7 +3,7 @@
 rm index.md 2>/dev/null
 rm README.md 2>/dev/null
 rm -rf images 2>/dev/null
-rm -rf files 2>dev/null
+rm -rf files 2>/dev/null
 
 php8.0 $(pwd)/cherrytreetomarkdown/cherrytomd.php -a -t $(pwd)/OSCP-Methodology.ctd $(pwd)
 git add -A
