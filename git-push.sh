37504fdfbe3e3db053b7f44ee937e1c8c1c771ce
Your Name <you@example.com>
2023-07-15 05:42:38 -0400

    Configured git script

--- git-push.sh
+++ git-push.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 php8.0 $(pwd)/cherrytreetomarkdown/cherrytomd.php -a -t $(pwd)/OSCP-Methodology.ctd $(pwd)
 git add *
-git commit -m ${1:-"Default Message"}
+git commit -m "${1:-'Default Message'}"
 read -s git_access_token
-echo https://$(git_access_token)@github.com/radoi-teodor/OSCP-Methodology.git
+echo https://$($git_access_token)@github.com/radoi-teodor/OSCP-Methodology.git
